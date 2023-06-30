e551362211564f708a1b5856d45e1b7eed005ef2
ManikaMalhotra <alice.ryan36912@gmail.com>
2023-06-30 07:38:59 +0530

    updated gitignore

--- src/apis/seeder/index.ts
+++ src/apis/seeder/index.ts
@@ -1,16 +1,13 @@
 import { Router } from 'express';
 import BooksRepository from '../../database/repository/Books';
 import { IFindBooksParams } from "../../types/params";
+import { seedGenres } from './genres';
+import { seedBooks } from './books';
 
 const router: Router = Router();
 
 router.get('/test', async (req, res) => {
-    const books = await BooksRepository.getBooks({
-        title: 'harry',
-        sortBy: 'dop',
-        limit: 10,
-        skip: 0,
-    } as IFindBooksParams);
+    const books = await seedBooks();
 
     res.json(books);
 });
--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,20 @@ import cors from 'cors';
 import connect from './loader/mongoose';
 import GoogleAuthRouter from './apis/googleAuth';
 import cookieParser from 'cookie-parser';
+import config from './config';
 
 const app: Application = express();
 const PORT = process.env.PORT || 8000;
+const { clientURI } = config;
 
 app.get('/', (req: Request, res: Response): void => {
     res.send('blabla');
 });
 
-app.use(cors());
+app.use(cors({
+    origin: clientURI,
+    credentials: true,
+}));
 app.use(cookieParser());
 app.use(express.json());
 app.use('/api', ApiRouter);
